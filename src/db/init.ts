d00d5b5ba77900b9545c4ee8a5acb862e4fcf083
Brambles-cat <gael.alejos05@gmail.com>
2024-07-16 19:28:15 -0700

    Build task for initializing db with sample data

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,3 +1,4 @@
+import { createHash } from "crypto";
 import { DB, tables } from "./database"
 
 async function setup() {
@@ -8,21 +9,17 @@ async function setup() {
       ${tables.artist_votes.candidate} TEXT PRIMARY KEY,  
       ${tables.artist_votes.votes} INTEGER  
     ); 
-    CREATE TABLE ${tables.artists.table_name} (
-      ${tables.artists.name} TEXT PRIMARY KEY,
-      ${tables.artists.country} TEXT,
-      ${tables.artists.song} TEXT
-    ); 
-    CREATE TABLE ${tables.countries.table_name} (
-      ${tables.countries.name} TEXT PRIMARY KEY,
-      ${tables.countries.password_hash} TEXT,
-      ${tables.countries.salt} TEXT,
-      ${tables.countries.session_id} TEXT
-
+    CREATE TABLE ${tables.candidates.table_name} (
+      ${tables.candidates.name} TEXT PRIMARY KEY,
+      ${tables.candidates.country} TEXT,
+      ${tables.candidates.song} TEXT,
+      ${tables.candidates.password_hash} TEXT,
+      ${tables.candidates.salt} TEXT,
+      ${tables.candidates.session_id} TEXT
     );
     CREATE TABLE ${tables.logins.table_name} (
-      ${tables.logins.password} TEXT PRIMARY KEY,
-      ${tables.logins.votes} INTEGER
+      ${tables.logins.login_code} TEXT PRIMARY KEY,
+      ${tables.logins.voted} INTEGER
     );
     CREATE TABLE ${tables.country_rankings.table_name} (
       ${tables.country_rankings.voter} TEXT PRIMARY KEY,
@@ -31,6 +28,18 @@ async function setup() {
     
   `);
 
+  if (process.argv.length > 2 && process.argv[2].toLowerCase() === 'use-sample-data')
+    for (let i = 1; i <= 12; ++i)
+      await DB.instance.db.run(
+        `INSERT INTO ${tables.candidates.table_name} (
+          ${tables.candidates.name}, ${tables.candidates.country}, ${tables.candidates.song},
+          ${tables.candidates.password_hash}, ${tables.candidates.salt}, ${tables.candidates.session_id}
+        ) VALUES (?, ?, ?, ?, ?, ?)`,
+        `candidate${i}`, `song${i}`, `country${i}`,
+        createHash('sha512').update(`${i}`).digest('hex'), 'salt', null
+      )
+
   await DB.instance.db.close();
 }
+
 setup().catch(err => console.error(err.message));
