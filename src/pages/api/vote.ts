33d8ab576664dcdc960c511956251484713dfeec
Brambles-cat <gael.alejos05@gmail.com>
2024-07-16 19:31:57 -0700

    Since voting uses all 10 votes at once, switched all remaining vote counters to just whether or not the user has voted

--- src/pages/api/vote.ts
+++ src/pages/api/vote.ts
@@ -8,9 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { votes, loginCode }: { votes: string[], loginCode: string} = req.body;
 
   if (!loginCode)
-    return res.status(400).json({ success: false, message: 'No login code found in cookies.' });
+    return res.status(400).json({ success: false, message: 'Not logged in' });
 
-  const valid_candidates = (await DB.instance.get_artists()).map(candidate => candidate.name)
+  const valid_candidates = (await DB.instance.get_candidates()).map(candidate => candidate.name)
 
   if (votes.length !== 10 || votes.find(candidate => !candidate || votes.indexOf(candidate) !== votes.lastIndexOf(candidate)))
     return res.json({ success: false, message: 'Impoper vote amount or duplicate votes found' })
@@ -18,12 +18,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (votes.find(candidate => !valid_candidates.includes(candidate)))
     return res.json({ success: false, message: 'Candidate not found' })
 
-  const remaining_votes = await DB.instance.get_remaining_votes(loginCode);
+  const has_voted = await DB.instance.get_has_voted(loginCode);
 
-  if (!remaining_votes)
-    return res.status(400).json({ success: false, message: 'No remaining votes or invalid login.' });
+  if (has_voted)
+    return res.status(400).json({ success: false, message: 'Already voted or invalid login.' });
 
   votes.forEach(async candidate => await DB.instance.cast_vote(candidate))
-  await DB.instance.update_remaining_votes(loginCode, 0);
+  await DB.instance.set_voted(loginCode);
   res.json({ success: true });
 }
